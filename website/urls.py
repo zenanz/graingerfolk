10d5bb6ed92661b74b8de75c7c4a21cd9509690e
zenanz <zenanz@student.unimelb.edu.au>
2017-12-13 13:35:06 +1100

    Headline section of collection page

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^interativeMap$', views.map, name='map'),
-    url(r'^collection$', views.collection, name='collection'),
+    path('', views.index, name='index'),
+    path('interativeMap', views.map, name='map'),
+    path('collection', views.collection, name='collection'),
+    path('collection/detail/<int:id>/', views.AtoM_API_CALL),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from http import client
+from urllib.parse import urlparse
+import json
 # Create your views here.
+
+ATOM_LOC = "10.100.148.152"
+API_KEY = '799cbb5aae706a3a'
 
 def index(request):
     return render(request, 'index.html',{'nbar': 'home'})
@@ -10,3 +15,19 @@ def map(request):
 
 def collection(request):
     return render(request, 'collection.html', {'nbar': 'collection'})
+
+def AtoM_API_CALL(request,id):
+    connection = client.HTTPConnection(ATOM_LOC)
+    connection.request('GET', '/api/informationobjects/creeping-jane-trail',headers={'REST-API-Key': API_KEY})
+    response = connection.getresponse()
+    js = json.loads(response.read())
+    music = urlparse("http://"+js["digital_object"]["reference_url"])._replace(netloc=ATOM_LOC).geturl()
+
+    connection.request('GET', '/api/informationobjects/creeping-jane', headers={'REST-API-Key': API_KEY})
+    response = connection.getresponse()
+    js = json.loads(response.read())
+    pdf = urlparse("http://" + js["digital_object"]["url"])._replace(netloc=ATOM_LOC).geturl()
+    img = urlparse("http://" + js["digital_object"]["reference_url"])._replace(netloc=ATOM_LOC).geturl()
+
+
+    return render(request, 'detail.html', {'nbar': 'collection', 'content': music, 'pdf':pdf ,'img':img})
